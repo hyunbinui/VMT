4ac24e5b5dc39780b1ed4fb9fe36990857e888bb
Hyunbin Jin <82128320+hyunbinui@users.noreply.github.com>
2022-12-16 13:55:16 +0900

    Update action_feature_extractor.py

--- action_feature_extractor/action_feature_extractor.py
+++ action_feature_extractor/action_feature_extractor.py
@@ -96,7 +96,6 @@ if __name__ == "__main__":
 
     parser.add_argument('--rgb_weights_path', type=str, default='./VMT-for-SUBS/action_feature_extractor/model_rgb.pth', help='Path to rgb model state_dict')
     parser.add_argument('--video_dir', type=str, default='./dataset/video_data', help='directory that contains video clips')
-    # parser.add_argument('--video_dir', type=str, default='/home/ubuntu/workspace/221122_vmt_hyunbin/DEAR/action_feature_extraction/code_kunst/', help='directory that contains video clips')
     parser.add_argument('--out_dir', type=str, default='./dataset/action_feature/', help='directory for extracted action features')
     args = parser.parse_args()
 
